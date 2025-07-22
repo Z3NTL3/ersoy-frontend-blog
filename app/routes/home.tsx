67b775888c52a6b4b7327536e3727dd2e120751a
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2025-07-22 18:07:21 +0200

    07/22/2025 18:07:21

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Skeleton } from "~/components/ui/skeleton";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Blog - Anasayfa - Mehdi Ersoy" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="w-screen h-screen flex grow justify-center items-center">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <Welcome/>
+    </div>
+  );
 }
