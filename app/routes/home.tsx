0b7c1184b184735de7e50962a15d65961a8ba016
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2025-07-22 22:35:26 +0200

    internet in turkey sucks couldnt go through docs/api ref so giving myself a brake

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-import { Skeleton } from "~/components/ui/skeleton";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,8 +10,7 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <div className="w-screen h-screen flex grow justify-center items-center">
-      <Skeleton className="h-12 w-12 rounded-full" />
-      <Welcome/>
+      
     </div>
   );
 }
---
+++ app/routes/layout.tsx
@@ -0,0 +1,14 @@
+import { type JSX } from "react"
+import { Outlet } from "react-router";
+export default function SecondRootLayout(): JSX.Element {
+    return (
+        <>
+            <title>Blog — Anasayfa - Mehdi Ersoy</title>
+            <meta name="description" content="Some meta shit here" />
+            <body>
+                
+                <Outlet />
+            </body>
+        </>
+    );
+};
