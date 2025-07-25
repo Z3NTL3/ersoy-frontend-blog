e55dea5c774f019a6f371b01882d5bbe83f3326e
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2025-07-25 18:13:35 +0200

    rm dep

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path"
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), cssInjectedByJsPlugin()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
