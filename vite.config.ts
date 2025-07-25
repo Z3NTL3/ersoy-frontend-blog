6a67858b8f8c859b5be2c13b1509afac19b1e1b7
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2025-07-25 18:28:40 +0200

    add babel

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,25 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path"
+import babel from "vite-plugin-babel";
 
+const ReactCompilerConfig = {} // todo
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(), 
+    reactRouter(), 
+    tsconfigPaths(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"], // if you use TypeScript
+        plugins: [
+          ["babel-plugin-react-compiler", ReactCompilerConfig],
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
