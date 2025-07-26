bda70f48dd871d560880a7b6958b8438eb89fa3a
z3ntl3 <z3ntl3@gmail.com>
2025-07-26 10:49:43 +0200

    Sat Jul 26 10:49:43 AM CEST 2025

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -45,6 +45,72 @@ const Navbar = ({children}) => {
         getter.change("dark")
     }
 
+    
+    let linksCls = "text-white dark:text-black"
+    let remove = "bg-black/80 border border-black/70 dark:bg-white/60 rounded-lg backdrop-blur-xl".split(" ")
+        
+    const rmClasses = (nav: HTMLElement) => {
+        for (let i = 0; i < remove.length; i++) {
+            let del = remove[i];
+            nav.classList.remove(del)
+        }
+    }
+    const linksManipulator = (links: HTMLElement, task: "add" | "del") => {
+        for (let i = 0; i < links.children.length; i++) {
+            let child = links.children.item(i)
+            if(task === "del")
+                child?.classList.remove(...linksCls.split(" "))
+            else
+                child?.classList.add(...linksCls.split(" "))
+        }
+    }
+
+    useEffect(() => {
+        var nav = document.getElementById("navbar")
+        var links = document.getElementById("nav-links")
+
+        document.addEventListener("scroll", (_) => {
+            let gapY = window.scrollY
+            let navState = 10
+
+            if(typeof nav === "undefined" || nav === null)
+                return
+            
+            if(typeof links === "undefined" || links === null)
+                return
+
+
+            if(gapY >= navState) {
+                let currentClasses = nav.getAttribute("class")
+                nav.setAttribute("class", `${currentClasses} bg-black/80 border border-black/70 dark:bg-white/60 rounded-lg backdrop-blur-xl`)
+        
+                  if(getter.currentTheme === "dark")
+                    document.getElementById("logo")?.setAttribute("src", "logo.svg")
+                else document.getElementById("logo")?.setAttribute("src", "logo-dark.svg")
+                for (let i = 0; i < links.children.length; i++) {
+                    let child = links.children.item(i)
+                    console.log(child)
+                    linksManipulator(links, "add")
+                }
+                return
+            }
+
+            rmClasses(nav)
+            linksManipulator(links, "del")
+
+            document.getElementById("logo")?.setAttribute("src", getCorrectScheme)
+            return () => {
+                // @ts-ignore
+                rmClasses(nav)
+                // @ts-ignore
+                linksManipulator(links, "del")
+                
+                document.getElementById("logo")?.setAttribute("src", getCorrectScheme)
+
+            }
+        })
+    })
+
     return (
         <NavbarSearchContext value={search}>
             <div className="flex justify-center" id="pagination"></div>
@@ -52,7 +118,7 @@ const Navbar = ({children}) => {
                 <div id="navbar" className="flex justify-start items-center w-[70%] h-[80px] p-4">
                     {/* logo */}
                     <div className="flex">
-                        <img width={150} src={getCorrectScheme} alt="logo" />
+                        <img id="logo" width={150} src={getCorrectScheme} alt="logo" />
                     </div>
                     {/* end */}
 
@@ -87,7 +153,6 @@ const Navbar = ({children}) => {
                 </div>
             </div>  
             {children}
-            <script src="/scripts/scroll.js" async onLoad={() => console.log("script loaded")}/>
         </NavbarSearchContext>
     );
 }
